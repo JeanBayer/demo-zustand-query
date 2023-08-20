d5a071ddbc06028b667bf91c23189d0c849227d4
jmoraba_bci <jhan.mora@bci.cl>
2023-08-19 23:16:42 -0400

    estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { Like, SimpleLanguage } from "./components";
 import { getLanguagesService, upLikeService } from "./services";
@@ -8,6 +8,8 @@ import "./App.css";
 
 function App() {
   const [languages, setLanguages] = useState<Language[]>();
+  const [order, setOrder] = useState<"asc" | "desc">("desc");
+  const [darkMode, setDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     getLanguagesService().then((response) => {
@@ -31,10 +33,36 @@ function App() {
     }
   };
 
+  const orderedLanguages = useMemo(() => {
+    if (!languages) return;
+    return [...languages]?.sort((a, b) => {
+      if (order === "asc") return a.likes - b.likes;
+      return b.likes - a.likes;
+    });
+  }, [order, languages]);
+
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? "App--dark" : ""}`}>
       <h1>Programming Languages</h1>
-      {languages?.map(({ author, id, likes, title }) => (
+      <div>
+        {order === "asc" ? (
+          <button onClick={() => setOrder("desc")}>DESC</button>
+        ) : (
+          <button onClick={() => setOrder("asc")}>ASC</button>
+        )}
+
+        <label htmlFor="checkbox" className={"checkbox--mode"}>
+          <span>{darkMode ? "Dark Mode ON" : "Click to enable Dark Mode"}</span>
+          <input
+            type="checkbox"
+            name="checkbox"
+            id="checkbox"
+            checked={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+          />
+        </label>
+      </div>
+      {orderedLanguages?.map(({ author, id, likes, title }) => (
         <div key={id}>
           <SimpleLanguage author={author} id={id} title={title} />
           <Like likes={likes} onLike={handleLike(id)} />
